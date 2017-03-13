74e89764bd0f74778382e1f2b6207cf85dfe489b
Jan Fitzner <jan.fitzner@macio.de>
2017-03-13 01:11:31 +0100

    Update Notification.java
    use .setAlarmClock instead of set, setExact and setExactAndAllowIdle

--- src/android/notification/Notification.java
+++ src/android/notification/Notification.java
@@ -179,18 +179,9 @@ public class Notification {
         if (isRepeating()) {
             getAlarmMgr().setRepeating(AlarmManager.RTC_WAKEUP,
                     triggerTime, options.getRepeatInterval(), pi);
-        } else {
-            if (android.os.Build.VERSION.SDK_INT >= 19 && android.os.Build.VERSION.SDK_INT < 23) {
-                getAlarmMgr().setExact(AlarmManager.RTC_WAKEUP, triggerTime, pi);
-            }
-            else if (android.os.Build.VERSION.SDK_INT >= 23)
-            {
-                getAlarmMgr().setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, triggerTime, pi);
-            }
-            else 
-            {
-                getAlarmMgr().set(AlarmManager.RTC_WAKEUP, triggerTime, pi);
-            }
+        }
+        else {
+            getAlarmMgr().setAlarmClock(new AlarmManager.AlarmClockInfo(triggerTime, pi), pi);
         }
     }
 
